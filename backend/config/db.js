608adaebcfb9705f54d539771c843b36bd6265e3
SazzadHossainPavel <sazzadhossain.pavel.3@gmail.com>
2023-09-29 18:19:30 +0600

    docker mongo issue fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,8 @@
 const mongoose = require("mongoose");
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const connectDB = async () => {
   try {
-    const mongoUrl = isProduction
-      ? process.env.MONGO_PROD_URI
-      : process.env.MONGO_URI;
-
-    const conn = await mongoose.connect(mongoUrl, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
